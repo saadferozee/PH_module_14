579a90a29364b5348838c24996cec96979385ea6
saadferozee <saadferozee@gmail.com>
2025-07-29 03:03:45 +0600

    added second task of practice Javascript

--- pages/2 BMI_Calculator.js
+++ pages/2 BMI_Calculator.js
@@ -13,3 +13,22 @@ Create a JavaScript program that calculates the Body Mass Index (BMI) and assign
 */
 
 
+let weight = 52;
+let height = 1.6;
+
+let bmi = weight / (height ^ 2);
+
+console.log ("Your BMI score is " + bmi.toFixed(3))
+
+if (bmi < 18.5) {
+    console.log ("You are Underweight")
+}
+else if (bmi >= 18.5 && bmi <= 24.9) {
+    console.log ("You are Normal")
+}
+else if (bmi >= 25 && bmi <= 29.9) {
+    console.log ("You are Overweight")
+}
+else {
+    console.log ("You are Obese")
+}
